c7b7b43ccc11c3dc4d15c1c797d1ba9fa7e51210
woaidouya123 <cfeigood@qq.com>
2020-05-03 11:24:06 +0800

    增加页面路由

--- mobileApp/src/routes.js
+++ mobileApp/src/routes.js
@@ -1,18 +1,35 @@
 import VueRouter from 'vue-router';
 import _404 from './utils/404.vue';
-import home from './home/home.vue'
+import home from './home/home.vue';
+import chapter from './home/chapter.vue';
+import content from './home/content.vue'
 const router = new VueRouter({
     routes:[
+        {
+            path:'/',
+            name:"主页面",
+            redirect:'/home'
+        },
         {
             path:'/home',
             name:"home",
             component:home
         },
         {
+            path:'/chapter/:name',
+            name:"chapter",
+            component:chapter
+        },
+        {
+            path:'/chapter/:name/content/:index',
+            name:"content",
+            component:content
+        },
+        {
             path:'/*',
             name:"404",
             component:_404
-        },
+        }
 
 
     ]
